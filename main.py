db033ded0ff360ef8a0e4ec9108917203facdb80
Gui David <62527620+gdavidss@users.noreply.github.com>
2023-11-26 22:09:02 -0800

    added types + other stuff

---
+++ main.py
@@ -0,0 +1,5 @@
+from simulator import SRS_Simulator
+
+user_one = SRS_Simulator(num_cards=3, initial_retrieviability=0.5, dt=0.1, verbose=True)
+
+num_cards = 3
--- simulator.py
+++ simulator.py
@@ -1,23 +1,33 @@
 import random
+from typing import List, Tuple
 
 class SRS_Simulator:
-    def __init__(self, cards, initial_state, dt):
-        # cards are dictionary of card numbers and their dependencies
-        self.cards = cards
-        self.num_cards = len(cards)
-        self.state = initial_state # initial retrievability of cards
+    def __init__(self, num_cards: int, initial_retrieviability: float, dt: float, verbose: bool = False):
+        self.verbose = verbose
+        self.num_cards = num_cards
+
+
+        last_review = [0.0 for _ in range(self.num_cards)]
+        retrievability = [initial_retrieviability for _ in range(self.num_cards)]
+        self.state = [retrievability, last_review]
         self.dt = dt  # time step
         self.t = 0   # global time
-    
-    def select_card(self):
+
+        if self.verbose:
+            print("Initialized SRS Simulator with {} cards".format(self.num_cards)
+                    + " and initial retrievability {}".format(initial_retrieviability)
+                    + " and time step {}".format(self.dt))
+            print("Initial state: {}".format(self.state))
+
+    def select_card(self) -> int:
         # Implement card selection policy
-        # This will be given by our POMDP policy
+        # This will be given by our MDP policy
         pass
 
-    def update_state(self, card, success):
+    def review_card(self, card: int, success: bool) -> None:
         if success:
             # If the review of a card was successful, set its retrievability to 1
-            self.state[card] = 1.0
+            self.state[0][card] = 1.0
         else:
             # INCOMPLETE
             # Ideally, we should be just decrease the stability of that card, 
@@ -29,13 +39,13 @@ class SRS_Simulator:
         #### 1) time since last review
         #### 2) stability of the card
 
-    def review_success_probability(self, card):
+    def review_success_probability(self, card: int) -> float:
         # if our card is dependent on some other, 
         # this should compute a probability based on the retrievability of our dependencies
-        # otherwise, we should define some arbirary probability of getting the card right
+        # otherwise, we should define some arbitrary probability of getting the card right
         pass
 
-    def step(self):
+    def step(self) -> List[List[float]]:
         # Choose a card to review
         card = self.select_card()
         
@@ -44,7 +54,7 @@ class SRS_Simulator:
         success = (random.random() < prob)
 
         # Update state
-        self.update_state(card, success)
+        self.review_card(card, success)
         
         # Increment time
         self.t += self.dt
