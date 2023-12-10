9d3e116ffd650fb04b91036bbde978cf274c00a4
Hannah Levin <hannahlevin1@gmail.com>
2023-12-10 11:46:42 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ NUM_CARDS = 4
 
 def main():
     qLearning = QLearning(numStates=NUM_CARDS)
-    simulator = SRS_Simulator(numCards=NUM_CARDS, model=qLearning)
+    simulator = SRS_Simulator(numCards=NUM_CARDS, model=qLearning, uniform = True)
     weights: np.ndarray = simulator.run(numEpisodes=10000)
+    print("Learned weights", weights)
+    print("When modeling stability as a uniform distribution:")
+    Evaluator.evaluate(numCards=NUM_CARDS, numEpisodes=10000, weights=weights)
 
+    simulator = SRS_Simulator(numCards=NUM_CARDS, model=qLearning, uniform= False)
+    weights: np.ndarray = simulator.run(numEpisodes=10000)
+    print("Learned weights", weights)
+    print("When modeling stability as a normal distribution:")
     Evaluator.evaluate(numCards=NUM_CARDS, numEpisodes=10000, weights=weights)
 
 if __name__ == "__main__":
