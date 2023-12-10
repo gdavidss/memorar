e7271c584c104049888c8d6272281881373db669
Hannah Levin <hannahlevin1@gmail.com>
2023-12-10 11:46:04 -0800

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -5,6 +5,8 @@ import random
 from collections import deque
 from ExperienceReplay import ExperienceReplay
 from tqdm import tqdm
+from collections import defaultdict
+import time
 
 EPSILON = 0.99
 BATCH_SIZE = 10
@@ -15,12 +17,13 @@ class SRS_Simulator():
     --------------------------
     This class implements a simulator for a Space Repetition System (SRS)
     """
-    def __init__(self, numCards: int, model: QLearning, verbose: bool = False):
+    def __init__(self, numCards: int, model: QLearning, verbose: bool = False, uniform: bool = False):
         self.model = model
         self.numCards = numCards
         self.epsilon = EPSILON
-        self.epsilon_min = 0.1
-        self.epsilon_decay = 0.995
+        self.uniform = uniform
+        self.epsilon_min = 0.1 # always explore at least 10 percent of the time
+        self.epsilon_decay = 0.995 # 5 percent decay in each run
         self.batchSize = BATCH_SIZE
         self.experienceDB = ExperienceReplay()
         self.state: List[Card] = [(Grade.Easy, 0) for _ in range(self.numCards)]
@@ -50,15 +53,34 @@ class SRS_Simulator():
         and exploring the state space. This does
         """
         # Create User
-        user = User(self.numCards)
+        user = User(numCards=self.numCards, uniform=self.uniform)
+        initial_user_stability = []
+        initial_user_stability.append([card_tuple[0] for card_tuple in user.cards])
+        #average_change_user_stability = initial_user_stability # change average change in user stability to dict and use default dict
+        prevStabilityChange = 0
+        user_flips_per_card = defaultdict(lambda: [0] * self.numCards)
+        userIndex = 0
          
         # TODO: Adapt to generate multiple users
-        for _ in tqdm(range(numEpisodes)):
+        for _ in tqdm(range(numEpisodes)): # every episode is looking at a single card
             if (user.hasAchievedMastery()):
-                user = User(self.numCards)
-                self.state: List[Card] = [(Grade.Easy, 0) for _ in range(self.numCards)]
+                print("user achieved mastery!")
+                user = User(numCards=self.numCards, uniform=self.uniform) # new user
+                self.state: List[Card] = [(Grade.Easy, 0) for _ in range(self.numCards)] # reset states
+                userIndex += 1
+                initial_user_stability.append([card_tuple[0] for card_tuple in user.cards])
+                #average_change_user_stability = initial_user_stability
+                card_tuples = user.cards
+                list_of_first_elements = [tup[0] for tup in card_tuples]
+                initial_user_stability.append(elem for elem in list_of_first_elements)
+
             # Choose a card to review
             action = self._getAction(self.state)
+            user_flips_per_card[userIndex][action] += 1
+
+            stabilityChange = prevStabilityChange - float(user.cards[action][0])
+            #average_change_user_stability[userIndex][action].append(stabilityChange)
+            prevStabilityChange = stabilityChange
 
             # Review card
             grade = user.reviewCard(action)
@@ -86,8 +108,16 @@ class SRS_Simulator():
                 self.model.updateWeights(self.state, action, reward, nextState)
             self.state = nextState
             
-            # epsilon decay after each run/episode:
+            # (update for epsilon greedy: epsilon decay after each run/episode:
             if self.epsilon > self.epsilon_min:
                 self.epsilon *= self.epsilon_decay
-        
+
+        print("num users: ", userIndex+1)
+
+        for userIndex, num_flips_per_card in user_flips_per_card.items():
+            # average change in stability per iteration per card
+            print(f"Num repeated for each card, User {userIndex}: {num_flips_per_card} with the following initial stability per card: {initial_user_stability[userIndex]}")
+            #print(f"Average change in stability for each card per user: {average_change_user_stability[userIndex]}")
+
         return self.model.weights
+
